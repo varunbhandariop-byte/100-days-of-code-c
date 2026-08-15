bfc6c10e33529f273d1d1d4c917672cfccd4261a
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-08-16 00:46:18 +0530

    Create Q3.c

---
+++ Day-02/Q3.c
@@ -0,0 +1,11 @@
+Write a program to input two numbers and display their sum
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+
+    return 0;
+}
