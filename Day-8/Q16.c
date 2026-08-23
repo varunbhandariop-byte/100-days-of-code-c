56e7cdc0800d05d531d9c0f16b0cb44b6ee10b80
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-08-23 11:12:02 +0530

    Create Q16.c

---
+++ Day-8/Q16.c
@@ -0,0 +1,18 @@
+Write a program to print the product of even numbers from 1 to n.
+  #include <stdio.h>
+
+int main() {
+    int n, i;
+    long long product = 1;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+
+    printf("Product of even numbers from 1 to %d = %lld", n, product);
+
+    return 0;
+}
