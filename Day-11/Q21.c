2f0e92fd5e2cc57452bd4c5379d40cfe70648822
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-08-24 21:16:51 +0530

    Create Q21.c

---
+++ Day-11/Q21.c
@@ -0,0 +1,19 @@
+Write a program to swap two numbers without using a third variable.
+  #include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping:\n");
+    printf("First number = %d\n", a);
+    printf("Second number = %d\n", b);
+
+    return 0;
+}
