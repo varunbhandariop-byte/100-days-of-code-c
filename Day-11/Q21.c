e499afe2c5d9bd1581124d036b6e5e163c3f511f
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-08-26 16:07:53 +0530

    Update Q21.c

--- Day-11/Q21.c
+++ Day-11/Q21.c
@@ -1,19 +1,52 @@
-Write a program to swap two numbers without using a third variable.
-  #include <stdio.h>
+Write a program to display the month name and number of days using switch-case for a given month number
+#include <stdio.h>
 
 int main() {
-    int a, b;
+    int month;
 
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
 
-    a = a + b;
-    b = a - b;
-    a = a - b;
-
-    printf("After swapping:\n");
-    printf("First number = %d\n", a);
-    printf("Second number = %d\n", b);
+    switch(month) {
+        case 1:
+            printf("January - 31 days");
+            break;
+        case 2:
+            printf("February - 28 or 29 days");
+            break;
+        case 3:
+            printf("March - 31 days");
+            break;
+        case 4:
+            printf("April - 30 days");
+            break;
+        case 5:
+            printf("May - 31 days");
+            break;
+        case 6:
+            printf("June - 30 days");
+            break;
+        case 7:
+            printf("July - 31 days");
+            break;
+        case 8:
+            printf("August - 31 days");
+            break;
+        case 9:
+            printf("September - 30 days");
+            break;
+        case 10:
+            printf("October - 31 days");
+            break;
+        case 11:
+            printf("November - 30 days");
+            break;
+        case 12:
+            printf("December - 31 days");
+            break;
+        default:
+            printf("Invalid month number");
+    }
 
     return 0;
 }
