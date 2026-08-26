9a7763f46ee4c7dc53b004b3274077fcb6b39594
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-08-26 15:52:00 +0530

    Update Q7.c

--- Day-04/Q7.c
+++ Day-04/Q7.c
@@ -1,28 +1,17 @@
-Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
-90-100: Grade A 
-80-89: Grade B 
-70-79: Grade C 
-60-69: Grade D 
-below 60: Grade F.
+Write a program to swap two numbers without using a third variable.
   
-  #include <stdio.h>
-
+#include <stdio.h>
 int main() {
-    int percentage;
+    int a, b;
 
-    printf("Enter percentage: ");
-    scanf("%d", &percentage);
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
 
-    if (percentage >= 90)
-        printf("Grade A");
-    else if (percentage >= 80)
-        printf("Grade B");
-    else if (percentage >= 70)
-        printf("Grade C");
-    else if (percentage >= 60)
-        printf("Grade D");
-    else
-        printf("Grade F");
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping: a = %d, b = %d", a, b);
 
     return 0;
 }
