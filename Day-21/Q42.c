bc80ce96f38a9d8e1c76c8c72ac80d953517d53d
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-09-07 22:16:01 +0530

    Create Q42.c

---
+++ Day-21/Q42.c
@@ -0,0 +1,29 @@
+Write a program to check if a number is a perfect number.
+  #include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+
+    if(sum == n)
+    {
+        printf("%d is a perfect number", n);
+    }
+    else
+    {
+        printf("%d is not a perfect number", n);
+    }
+
+    return 0;
+}
