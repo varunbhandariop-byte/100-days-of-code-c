2efe1a81277025880894644e58cbe7f8807072a8
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-09-07 13:50:50 +0530

    Create Q37.c

---
+++ Day-19/Q37.c
@@ -0,0 +1,26 @@
+Write a program to find the LCM of two numbers.
+  #include <stdio.h>
+
+int main()
+{
+    int a, b, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    lcm = (a > b) ? a : b;
+
+    while(1)
+    {
+        if(lcm % a == 0 && lcm % b == 0)
+        {
+            break;
+        }
+
+        lcm++;
+    }
+
+    printf("LCM = %d", lcm);
+
+    return 0;
+}
