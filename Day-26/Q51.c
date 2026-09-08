b92e21e36f3087e0185823971ad11e78a58fd546
varunbhandariop-byte <varunbhandari.op@gmail.com>
2026-09-08 20:09:09 +0530

    Create Q51.c

---
+++ Day-26/Q51.c
@@ -0,0 +1,27 @@
+Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+  #include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 5; i >= 1; i--)
+    {
+        // spaces
+        for(j = 1; j < i; j++)
+            printf(" ");
+
+        // numbers
+        for(j = i; j <= 5; j++)
+            printf("%d", j);
+
+        printf("\n");
+    }
+
+    return 0;
+}
